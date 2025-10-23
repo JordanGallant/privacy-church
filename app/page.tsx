0d5d0a0e37b0907d2f9eca65e761c09d901e6474
JordanGallant <39-12602@sae.edu.za>
2025-10-24 00:02:20 +0200

    adding assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Navbar from "./_components/nav";
+import Main from "./_components/sections/main";
 
 
 export default function Home() {
   return (
-<>
+    <>
+<Main/>
 <Navbar/>
+
 </>
   );
 }
