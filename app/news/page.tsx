8afe05020b8238f776b41f4a3885b65ad72a5728
JordanGallant <39-12602@sae.edu.za>
2025-10-24 15:12:23 +0200

    news page

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,19 +1,25 @@
-import News from "../_components/news"
-import Navbar from "../_components/nav"
+import News from "../_components/news";
+import Navbar from "../_components/nav";
 
 export default function NewspPage() {
   return (
     <>
-        <Navbar hideLogo={false} />
-  <h1 className="text-6xl font-bold mb-4 italic font-[var(--font-gt-planar-light)]">
+      <Navbar hideLogo={false} />
+
+      <div className="px-6 my-8">
+        <h1 className="text-5xl italic font-bold mb-2 leading-tight font-[family-name:var(--font-gt-planar-menu)]">
           News
         </h1>
-        
-        <p className="text-base text-gray-600 mb-8 font-[var(--font-gt-planar-light)]">
+
+        <p
+          className="text-base md:text-lg leading-relaxed font-[family-name:var(--font-dm-mono)]"
+          style={{ color: "#717171" }}
+        >
           Fresh sins, big wins and ongoing acts of surveillance disguised as progress.
         </p>
+      </div>
 
-    <News/>
+      <News />
     </>
-  )
+  );
 }
