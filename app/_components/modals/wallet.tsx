b7a5505fd3b5ee758f869f93f0c792d2f86020b5
JordanGallant <39-12602@sae.edu.za>
2025-11-07 07:56:32 +0100

    log current wallet

--- app/_components/modals/wallet.tsx
+++ app/_components/modals/wallet.tsx
@@ -1,8 +1,18 @@
 import { useEthers } from '@usedapp/core';
+import { useEffect } from 'react';
 
 export const Connect = () => {
   const { account, deactivate, activateBrowserWallet } = useEthers();
-  // 'account' being undefined means that we are not connected.
+
+  // Log the connected account to the console whenever it changes
+  useEffect(() => {
+    if (account) {
+      console.log('Connected account:', account);
+    } else {
+      console.log('No account connected');
+    }
+  }, [account]);
+
   if (account)
     return (
       <button className="connect" onClick={() => deactivate()}>
